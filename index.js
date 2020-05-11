8ac2a3e39b6961a775cb77502e4a670f5e21484e
Shreyans Jain <shreyans.jain@olx.com>
2020-05-11 23:36:52 +0200

    remove node_modules, create ha hello-world addon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var noble = require('noble');
+var noble = require("@abandonware/noble");
 var poweredOn = false;
-var serviceUUids = ['ffb0'];
+var serviceUUids = ["ffb0"];
 // noble.startScanning([],true)
 // setTimeout(function() {
 // 	if(poweredOn === true){
@@ -10,38 +10,40 @@ var serviceUUids = ['ffb0'];
 // },50000)
 // var color = [0x01,0xf0,0x00,0x06,0x00,0x0a,0x03,0x00,0x01,0x01,0x00,0xRR,0xGG,0xBB,0x00,0xBR,0x00,0x00];
 
-var color = [0xd1,0xff,0x00,0x00];
+var color = [0xd1, 0xff, 0x00, 0x00];
 
-noble.on('stateChange', function(state) {
-	console.log(state);
-	if(state ==='poweredOn'){
-		noble.startScanning(serviceUUids,false);
-	}
-	// console.log(state);
+noble.on("stateChange", function (state) {
+  console.log(state);
+  if (state === "poweredOn") {
+    noble.startScanning(serviceUUids, false);
+  }
+  // console.log(state);
 });
 
-
-
-noble.on('discover', function(peripheral) {
-    console.log('Found device with local name: ' + peripheral.advertisement.localName);
-    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
-    console.log();
-    noble.stopScanning();
-    console.log("connecting");
-    peripheral.connect(function(err){
-    	 console.log('connected to peripheral: ' + peripheral.uuid);
-    	 peripheral.discoverServices(['ffb0'], function(error, services) {
-    	 	var service = services[0];
-    	 	service.discoverCharacteristics(['ffb2'], function(error, characteristics) {
-    	 		var colorcharacteristic = characteristics[0]
-		        colorcharacteristic.write(new Buffer(color), true, function(error) {
-		          console.log('set alert level to mid (1)');
-		        });
-
-		    });
-
-    	 });
-    })
-
-
+noble.on("discover", function (peripheral) {
+  console.log(
+    "Found device with local name: " + peripheral.advertisement.localName
+  );
+  console.log(
+    "advertising the following service uuid's: " +
+      peripheral.advertisement.serviceUuids
+  );
+  console.log();
+  noble.stopScanning();
+  console.log("connecting");
+  peripheral.connect(function (err) {
+    console.log("connected to peripheral: " + peripheral.uuid);
+    peripheral.discoverServices(["ffb0"], function (error, services) {
+      var service = services[0];
+      service.discoverCharacteristics(["ffb2"], function (
+        error,
+        characteristics
+      ) {
+        var colorcharacteristic = characteristics[0];
+        colorcharacteristic.write(new Buffer(color), true, function (error) {
+          console.log("set alert level to mid (1)");
+        });
+      });
+    });
+  });
 });
